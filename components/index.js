31791feb8df94db59ae5496448497742eacc0040
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-10-12 02:30:35 +0300

    feat: build NFT Details component

--- components/index.js
+++ components/index.js
@@ -1,5 +1,15 @@
 import HomeHeader from './HomeHeader';
 import NFTCard from './NFTCard';
 import FocusedStatusBar from './FocusedStatusBar';
+import DetailsBid from './DetailsBid';
+import DetailsDesc from './DetailsDesc';
+import { SubInfo } from './SubInfo';
 
-export { HomeHeader, NFTCard, FocusedStatusBar };
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo,
+};
